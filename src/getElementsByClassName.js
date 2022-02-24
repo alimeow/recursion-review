80677fe918948c2d1616d05301f8f7380909fec3
Anny Wang <astrjc.anny@gmail.com>
2022-02-23 17:15:02 -0800

    passed the getElementsByClassNameSpec

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,20 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var content = document.body;
+  var result = [];
+
+  var checkClass = function(element) {
+    if (element.classList !== undefined && element.classList.contains(className)) {
+      result.push(element);
+    }
+
+    if (element.hasChildNodes() === true) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        checkClass(element.childNodes[i]);
+      }
+    }
+  };
+  checkClass(content);
+  return result;
 };
